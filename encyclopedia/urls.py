7e5e3269059fc25466ddfb91518b2397b086f5d3
Phillip <philliplee515@gmail.com>
2022-04-28 16:01:41 +0900

    Entry Page/Index Page/Search/New Page/Edit Page

---
+++ encyclopedia/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("title/<str:title_>", views.title, name="title"),
+    path("create_new_page", views.newpage, name="newpage"),
+    path("title/<str:title>/editpage", views.editpage, name="editpage"),
+    path("title/<str:title>/editpage/edit", views.edit, name="edit"),
+    path("search", views.search, name="search"),
+]
---
+++ encyclopedia/views.py
@@ -0,0 +1,105 @@
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    # if request.method == "POST":
+    #     results = []
+    #     possibles = []
+    #     searched = request.POST["q"]
+    #     for e in util.list_entries():
+    #         if searched.lower() in e.lower():
+    #             results.append(searched)
+    #             possibles.append(e)
+    #     if len(results) == 1 and results[0].lower() == possibles[0].lower():
+    #         return render(request, "encyclopedia/title.html", {
+    #             "title": possibles[0],
+    #             "entry": util.get_entry(possibles[0])
+    #         })
+    #     return render(request, "encyclopedia/index.html", {
+    #         "entries": possibles,
+    #         "title": "Search Results"
+    #     })
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+        "title": "All Pages"
+    })
+
+
+def search(request):
+    if request.method == "POST":
+        results = []
+        possibles = []
+        searched = request.POST["q"]
+        for e in util.list_entries():
+            if searched.lower() in e.lower():
+                results.append(searched)
+                possibles.append(e)
+        if len(results) == 1 and results[0].lower() == possibles[0].lower():
+            return render(request, "encyclopedia/title.html", {
+                "title": possibles[0],
+                "entry": util.get_entry(possibles[0])
+            })
+        return render(request, "encyclopedia/index.html", {
+            "entries": possibles,
+            "title": "Search Results"
+        })
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+        "title": "All Pages"
+    })
+        
+
+
+
+def title(request, title_):
+
+    if not util.get_entry(title_):
+        return render(request, "encyclopedia/title.html", {
+            "entry": f'"{title_}" does not exist'
+        })
+
+    return render(request, "encyclopedia/title.html", {
+        "title": title_,
+        "entry": util.get_entry(title_)
+    })
+
+    
+
+def newpage(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+
+        if util.get_entry(title):
+            return render(request, "encyclopedia/newpage.html", {
+                "message": "Entry already exists!"
+            })
+
+        util.save_entry(title, content)
+
+        return HttpResponseRedirect(reverse("index"))
+
+    return render(request, "encyclopedia/newpage.html")
+
+
+def editpage(request, title):
+    return render(request, "encyclopedia/editpage.html", {
+        "title": title,
+        "content": util.get_entry(title),
+    })
+
+def edit(request, title):
+    if request.method == "POST":
+        new_content = request.POST['content']
+        util.save_entry(title, new_content)
+
+
+    return HttpResponseRedirect(reverse('index'))
+
+    
+    
+
